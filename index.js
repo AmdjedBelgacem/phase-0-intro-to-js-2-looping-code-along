b98c577ac1fa73fe1708460eb574597cdb6cc32e
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-05-06 23:37:32 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+let arr = [];
+function writeCards(name, reason){
+    name.forEach(element => {
+        arr.push(`Thank you, ${element}, for the wonderful ${reason} gift!`);
+    })
+    return arr;
+}
+
+function countDown(num){
+    while (0 <= num){
+        console.log(num);
+        num--;
+    }
+}
